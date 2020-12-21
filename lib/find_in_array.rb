4faae0ba7040a484eaaf2f1eac574be0964ce51c
eager-monitor-013e <phil.haines92@gmail.com>
2020-12-21 18:58:41 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   array.each do_with_index |element, idx|
-  if array[idx] == value_to_find
+  if array[element] == value_to_find
     return array[idx]
   end
   
